08853fdd748bb93e427d875c3174331b30fb04f6
Crutcher <vlad.goloskov@icloud.com>
2020-07-02 19:11:10 +0300

    Project init. Create isDivide() function.

---
+++ Lesson 2/Lesson 2/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Lesson 2
+//
+//  Created by Влад Голосков on 02.07.2020.
+//  Copyright © 2020 Владислав Голосков. All rights reserved.
+//
+
+import Foundation
+
+func isDivide(_ divident: Int, _ divider: Int) -> Bool {
+    if divident % divider == 0 {
+        return true
+    } else {
+        return false
+    }
+}
