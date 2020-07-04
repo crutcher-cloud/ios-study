dc773c94bb73c7c8101c353086e88063eeff29db
Crutcher <vlad.goloskov@icloud.com>
2020-07-04 19:05:18 +0300

    Tasks complete.

--- 3l_ГолосковВладислав.playground/Contents.swift
+++ 3l_ГолосковВладислав.playground/Contents.swift
@@ -5,7 +5,72 @@ enum VehicleInteraction {
 }
 
 enum CargoInteraction {
-    case addCargo, reduceCargo
+    case addCargo(weight: Int)
+    case reduceCargo(weight: Int)
+}
+
+struct lightVehicle {
+    var brandName: String
+    var year: Int
+
+    var isEngineStart: Bool
+    var isWindowsOpen: Bool
+    
+    var maxPassengerCount: UInt8
+    var currentPassengerCount: UInt8
+    
+    var trunkCapacity: Int
+    var filledSpace: Int
+    var freeSpace: Int {
+        get {
+            return trunkCapacity - filledSpace
+        }
+    }
+    
+    func printPassangersInfo() {
+        print("\(brandName):\nВсего пассажирских мест: \(maxPassengerCount)\nЗанято мест: \(currentPassengerCount)\nСвободно мест: \(maxPassengerCount - currentPassengerCount)\n")
+    }
+    
+    func printSpaceInfo() {
+        print("\(brandName):\nСвободно: \(freeSpace) кг.\nЗанято: \(filledSpace) кг.\n")
+    }
+    
+    mutating func carInteraction(actionName action: VehicleInteraction) {
+        switch action {
+        case .startEngine:
+            isEngineStart = true
+            print("\(brandName): Двигатель запущен!")
+        case .stopEngine:
+            isEngineStart = false
+            print("\(brandName): Двигатель остановлен!")
+        case .openWindows:
+            isWindowsOpen = true
+            print("\(brandName): Окна открыты!")
+        case .closeWindows:
+            isWindowsOpen = false
+            print("\(brandName): Окна закрыты!")
+        }
+    }
+    
+    mutating func cargoInteraction(actionName action: CargoInteraction) {
+           switch action {
+           case .addCargo(weight: let weight):
+               if filledSpace + weight < trunkCapacity {
+                   filledSpace += weight
+                   print("\(brandName): \(weight) кг. груза добавлено!\n")
+               } else {
+                   print("\(brandName): Ошибка! Слишком большое количество груза! Свободно \(freeSpace) кг.\n")
+               }
+           case .reduceCargo(weight: let weight):
+               if filledSpace - weight >= 0 {
+                   filledSpace -= weight
+                   print("\(brandName): \(weight) кг. груза убрано!\n")
+               } else {
+                   print("\(brandName): Такого количества груза в машине нет! Убран весь имеющийся груз.\n")
+                   filledSpace = 0
+               }
+           }
+       }
 }
 
 struct heavyVehicle {
@@ -15,52 +80,74 @@ struct heavyVehicle {
     var isEngineStart: Bool
     var isWindowsOpen: Bool
     
-    var bodySpace: Int
+    var bodyCapacity: Int
     var filledSpace: Int
     var freeSpace: Int {
         get {
-            return bodySpace - filledSpace
+            return bodyCapacity - filledSpace
         }
     }
     
-    func printSpace() {
-        print("Свободно: \(freeSpace) кг.\nЗанято: \(filledSpace) кг.\n")
+    func printSpaceInfo() {
+        print("\(brandName):\nСвободно: \(freeSpace) кг.\nЗанято: \(filledSpace) кг.\n")
     }
     
     mutating func carInteraction(actionName action: VehicleInteraction) {
         switch action {
         case .startEngine:
             isEngineStart = true
-            print("Двигатель запущен!")
+            print("\(brandName): Двигатель запущен!")
         case .stopEngine:
             isEngineStart = false
-            print("Двигатель остановлен!")
+            print("\(brandName): Двигатель остановлен!")
         case .openWindows:
             isWindowsOpen = true
-            print("Окна открыты!")
+            print("\(brandName): Окна открыты!")
         case .closeWindows:
             isWindowsOpen = false
-            print("Окна закрыты!")
+            print("\(brandName): Окна закрыты!")
         }
     }
     
-    mutating func cargoInteraction(actionName action: CargoInteraction, cargoAmount: Int) {
+    mutating func cargoInteraction(actionName action: CargoInteraction) {
         switch action {
-        case .addCargo:
-            if filledSpace + cargoAmount < bodySpace {
-                filledSpace += cargoAmount
-                print("\(cargoAmount) кг. груза добавлено!\n")
+        case .addCargo(weight: let weight):
+            if filledSpace + weight < bodyCapacity {
+                filledSpace += weight
+                print("\(brandName): \(weight) кг. груза добавлено!\n")
             } else {
-                print("Ошибка! Слишком большое количество груза! Свободно \(freeSpace) кг.\n")
+                print("\(brandName): Ошибка! Слишком большое количество груза! Свободно \(freeSpace) кг.\n")
             }
-        case .reduceCargo:
-            if filledSpace - cargoAmount >= 0 {
-                filledSpace -= cargoAmount
-                print("\(cargoAmount) кг. груза убрано!\n")
+        case .reduceCargo(weight: let weight):
+            if filledSpace - weight >= 0 {
+                filledSpace -= weight
+                print("\(brandName): \(weight) кг. груза убрано!\n")
             } else {
-                print("Такого количества груза в машине нет! Убран весь имеющийся груз.\n")
+                print("\(brandName): Такого количества груза в машине нет! Убран весь имеющийся груз.\n")
                 filledSpace = 0
             }
         }
     }
 }
+
+var firstTruck = heavyVehicle(brandName: "Volvo", year: 2012, isEngineStart: false, isWindowsOpen: false, bodyCapacity: 4000, filledSpace: 700)
+firstTruck.carInteraction(actionName: .startEngine)
+firstTruck.carInteraction(actionName: .openWindows)
+
+firstTruck.cargoInteraction(actionName: .addCargo(weight: 500))
+firstTruck.printSpaceInfo()
+
+var secondTruck = heavyVehicle(brandName: "Kamaz", year: 1989, isEngineStart: true, isWindowsOpen: true, bodyCapacity: 7000, filledSpace: 1000)
+secondTruck.cargoInteraction(actionName: .reduceCargo(weight: 250))
+secondTruck.printSpaceInfo()
+
+secondTruck.carInteraction(actionName: .closeWindows)
+secondTruck.carInteraction(actionName: .stopEngine)
+
+var sportCar = lightVehicle(brandName: "BMW", year: 2019, isEngineStart: false, isWindowsOpen: false, maxPassengerCount: 2, currentPassengerCount: 1, trunkCapacity: 200, filledSpace: 5)
+sportCar.carInteraction(actionName: .startEngine)
+sportCar.carInteraction(actionName: .openWindows)
+sportCar.printPassangersInfo()
+
+sportCar.cargoInteraction(actionName: .addCargo(weight: 10))
+sportCar.printSpaceInfo()
