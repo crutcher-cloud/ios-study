33d1f7cee98d7e31fa0da8b9ca1d829678c8e838
Crutcher <vlad.goloskov@icloud.com>
2020-07-19 16:34:26 +0300

    Tasks complete.

--- extensions.swift
+++ extensions.swift
@@ -17,14 +17,14 @@ extension Employee: CustomStringConvertible {
 }
 
 extension Company {
-    func getEmployeeInfo(employeeName name: String) -> (Employee?, companyErrors?) {
+    func getEmployeeInfo(employeeName name: String) -> (Employee?, CompanyErrors?) {
         guard let currentEmployee = employeeList[name] else {
             return (nil, .undefinedEmployee)
         }
         return (currentEmployee, nil)
     }
     
-    func getEmployeeWithMaxSalary() -> (Employee?, companyErrors?) {
+    func getEmployeeWithMaxSalary() -> (Employee?, CompanyErrors?) {
         guard employeeList.count != 0 else {
             return (nil, .employeeListIsEmpty)
         }
@@ -39,5 +39,54 @@ extension Company {
         }
         
         return (employeeList[searchedEmployee], nil)
+    }
+}
+
+//Extensions for Task 2.
+
+extension Item: CustomStringConvertible {
+    var description: String {
+        return "Наименование: \(product.name)\nКоличество: \(count)\nСтоимость: \(price)\n"
+    }
+}
+
+extension Product: CustomStringConvertible {
+    var description: String {
+        return "\(name)"
+    }
+}
+
+func addItemInTo(_ store: Store, itemName: String, count: UInt, price: UInt) {
+    do {
+        try store.addItem(title: itemName, count: count, price: price)
+        print("\(itemName) внесен в список товаров!\n")
+    } catch StoreErrors.overAdd {
+        print("\(itemName) уже есть в списке товаров!\n")
+    } catch let error {
+        print(error.localizedDescription)
+    }
+}
+
+func removeItemFrom(_ store: Store, itemName: String) {
+    do {
+        try store.removeItem(title: itemName)
+        print("\(itemName) удален из списка товаров!\n")
+    } catch StoreErrors.undefinedItem {
+        print("\(itemName) отсутствует в списке товаров!\n")
+    } catch let error {
+        print(error.localizedDescription)
+    }
+}
+
+func sellItemFrom(_ store: Store, itemName: String, customerMoney: UInt) {
+    do {
+        let product = try store.sellItem(itemName: itemName, customerMoney: customerMoney)
+        print("\(product) успешно приобретен!\nПоздравляем с покупкой!\n")
+    } catch StoreErrors.undefinedItem {
+        print("Такого товара в магазине нет :(\n")
+    } catch StoreErrors.insufficientFunds(moneyNeeded: let moneyNeeded) {
+        print("Недостаточно средств. Необходимо еще \(moneyNeeded)")
+    } catch let error {
+        print(error.localizedDescription)
     }
 }
--- main.swift
+++ main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-//Task 1. Come up with a class whose methods may fail and return either a value or an Error?Implement their call and process the result of the method using the if let or guard let construct.
+//Task 1. Come up with a class whose methods may fail and return either a value or an Error? Implement their call and process the result of the method using the if let or guard let construct.
 
 struct Employee {
     let name: String
@@ -16,7 +16,7 @@ struct Employee {
     var salary: UInt
 }
 
-enum companyErrors: Error {
+enum CompanyErrors: Error {
     case undefinedEmployee
     case employeeListIsEmpty
 }
@@ -54,3 +54,66 @@ if let employee = someEmployee.0 {
 } else if let error = someEmployee.1 {
     print("Произошла ошибка: \(error)\n")
 }
+
+//Task 2. Create a class whose methods can throw out errors. Implement several throws-functions. Call them and process the result using the try/catch construct.
+
+struct Item {
+    var count: UInt
+    var price: UInt
+    let product: Product
+}
+
+struct Product {
+    let name: String
+}
+
+enum StoreErrors: Error {
+    case undefinedItem
+    case overAdd
+    case insufficientFunds (moneyNeeded: UInt)
+}
+
+class Store {
+    var priceList = [String: Item]()
+    
+    func addItem(title: String, count: UInt, price: UInt) throws {
+        guard priceList[title] == nil else {
+            throw StoreErrors.overAdd
+        }
+        priceList[title] = Item(count: count, price: price, product: Product(name: title))
+    }
+    
+    func removeItem(title: String) throws {
+        guard priceList[title] != nil else {
+            throw StoreErrors.undefinedItem
+        }
+        priceList.removeValue(forKey: title)
+    }
+    
+    func sellItem(itemName: String, customerMoney: UInt) throws -> Product {
+        guard let item = priceList[itemName] else {
+            throw StoreErrors.undefinedItem
+        }
+        
+        guard customerMoney >= item.price else {
+            throw StoreErrors.insufficientFunds(moneyNeeded: item.price - customerMoney)
+        }
+        
+        var soldItem = item
+        soldItem.count -= 1
+        priceList[itemName] = soldItem
+        
+        return soldItem.product
+    }
+}
+
+var smartphoneStore = Store()
+
+addItemInTo(smartphoneStore, itemName: "iPhone SE(2016)", count: 15, price: 12000)
+addItemInTo(smartphoneStore, itemName: "iPhone 11 Pro", count: 10, price: 85000)
+addItemInTo(smartphoneStore, itemName: "iPhone 11", count: 10, price: 60000)
+
+removeItemFrom(smartphoneStore, itemName: "iPhone 16")
+
+sellItemFrom(smartphoneStore, itemName: "iPhone 11 Pro", customerMoney: 90000)
+sellItemFrom(smartphoneStore, itemName: "iPhone 16", customerMoney: 1000000)
